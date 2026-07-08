11433019b9c7b351df7d53b178b6bb3b56031fc0
Abolfazl-Bahrami <227335344+Abolfazl-Bahrami@users.noreply.github.com>
2026-07-08 17:41:01 +0330

    Add Css code and picture

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let score = JSON.parse(localStorage.getItem('score')) || {win : 0 , lose : 0 , tie : 0};
+let score = JSON.parse(localStorage.getItem('score')) || { win: 0, lose: 0, tie: 0 };
 
 
-function playGame(chose){
+function playGame(chose) {
     let computerMoveGame = computeMove();
 
     //game
     let result;
-    if(chose === 'Rock'){
-    result = computerMoveGame === 'Rock' ? 'Tie' : (computerMoveGame === 'Paper' ? 'Lose' : 'Win');
-    }else if(chose === 'Paper'){
-    result = computerMoveGame === 'Paper' ? 'Tie' : (computerMoveGame === 'Scissors' ? 'Lose' : 'Win');
-    }else {
+    if (chose === 'Rock') {
+        result = computerMoveGame === 'Rock' ? 'Tie' : (computerMoveGame === 'Paper' ? 'Lose' : 'Win');
+    } else if (chose === 'Paper') {
+        result = computerMoveGame === 'Paper' ? 'Tie' : (computerMoveGame === 'Scissors' ? 'Lose' : 'Win');
+    } else {
         result = computerMoveGame === 'Scissors' ? 'Tie' : (computerMoveGame === 'Rock' ? 'Lose' : 'Win');
     }
 
@@ -18,39 +18,39 @@ function playGame(chose){
     document.querySelector('.js-result').innerHTML = `You ${result}`;
 
     //print why
-    document.querySelector('.js-why').innerHTML = `You ${chose} - Computer ${computerMoveGame}`;
+    document.querySelector('.js-why').innerHTML = `You <img src="./emoji/${chose}-emoji.png" class="move-icon">  <img src="./emoji/${computerMoveGame}-emoji.png" class="move-icon"> Computer`;
 
     //score
-    if(result === 'Win'){
+    if (result === 'Win') {
         score.win++;
-    }else if(result === 'Lose'){
+    } else if (result === 'Lose') {
         score.lose++;
-    }else score.tie++;
+    } else score.tie++;
 
     //save score
-    localStorage.setItem('score' ,  JSON.stringify(score) );
+    localStorage.setItem('score', JSON.stringify(score));
 
     updateScore();
 }
 
-function updateScore(){
+function updateScore() {
     document.querySelector('.js-score').innerHTML = `Win : ${score.win} , Lose : ${score.lose} , Tie : ${score.tie}`;
 }
-function computeMove(){
-    let num = Math.random() , move;
-    if(num < 1/3){
+function computeMove() {
+    let num = Math.random(), move;
+    if (num < 1 / 3) {
         move = 'Rock';
-    }else if(num >1/3 && num <2/3){
+    } else if (num > 1 / 3 && num < 2 / 3) {
         move = 'Paper';
-    }else move = 'Scissors';
+    } else move = 'Scissors';
 
     return move;
 }
 
-function restScore(){
+function restScore() {
     score.win = 0;
-    score.lose = 0 ;
-    score.tie =  0 ;
+    score.lose = 0;
+    score.tie = 0;
     updateScore();
     localStorage.removeItem('score');
 }
